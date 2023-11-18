8c36150714cdb4d31dcccfb2c0327ae5a44bb2d7
milunadev <misusuyatec@gmail.com>
2023-11-18 15:10:46 -0500

    Subiendo el pipeline folder

---
+++ copilot/pipelines/app3copilot-main/test_website.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# URL de tu servicio
+URL="http://tu-servicio.com"
+
+# Descarga el contenido de la página
+content=$(curl -s $URL)
+
+# Comprueba si el contenido cumple con las expectativas
+echo "Comprobando H1..."
+if echo "$content" | grep -q "<h1>HOLA :D </h1>"; then
+    echo "Prueba H1 pasada."
+else
+    echo "Prueba H1 fallida."
+    exit 1
+fi
+
+echo "Comprobando H2..."
+if echo "$content" | grep -q "MILUNADEV"; then
+    echo "Prueba H2 pasada."
+else
+    echo "Prueba H2 fallida."
+    exit 1
+fi
+
+echo "Comprobando imagen..."
+if echo "$content" | grep -q 'img .* width="350"'; then
+    echo "Prueba de imagen pasada."
+else
+    echo "Prueba de imagen fallida."
+    exit 1
+fi
+
+echo "Todas las pruebas pasaron."
